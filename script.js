9eb1bb3d6935f2614d0c9015b66fa10b8dc5ac9d
romanchik4261 <romanchik4261@mail.ru>
2021-07-20 18:56:41 +0300

    Homework3

--- script.js
+++ script.js
@@ -1,15 +1,50 @@
-alert("Привет, давай посчитаем температуру по Фаренгейту");
+//1 задание
+let number = 0;
+  while (number < 100) {
+    if (simple(number)) {
+      console.log(number);
+    }
+    number++;
+  }
 
-let Tc = prompt("Задай температуру градусов по Цельсию, которую нужно первести в Фаренгейт");
+  function simple(number) {
+      if (number < 2) {
+          return false;
+      }
 
-let Tf = (9 / 5) * Tc + 32;
+      for (let i = 2; i <= Math.sqrt(number); i++) {
+          if (number % i === 0) {
+              return false;
+          }
+      }
+      return true;
+      
+  }
 
-alert("Отлично " + Tc + " градусов по Цельсию" + " это " + Tf + " градусов по Фаренгейту");
+  // 2 задание
 
-let admin;
-let name = "Василий";
+  const cart = [
+      ["Платье", 2000, 2],
+      ["Юбка", 1000, 2],
+      ["Футболка", 500, 2],
+      ["Штаны", 2000, 2],
+      ["Жакет", 1500, 2]
+  ]
 
-admin = name;
-alert ("Отлично " + admin);
-
-alert(1000 + "108");
+  let count = cart.map(function(num) {
+      return num[1] * [2];
+  });
+  alert(count);
+    
+  cart[0].splice(1, 2, (count[0]));
+  cart[1].splice(1, 2, (count[1]));
+  cart[2].splice(1, 2, (count[2]));
+  cart[3].splice(1, 2, (count[3]));
+  cart[4].splice(1, 2, (count[4]));
+  alert(cart);
+  
+  const result = cart.reduce(function(sum, current) {
+      return sum + current[1];
+    }, 0);
+    
+    alert("Итого в корзине " + cart.length + " товаров на сумму " + result + " рублей.");
