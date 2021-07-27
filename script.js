64ad40c8b6f32f29da910fc62b6a4c0dd26cf248
romanchik4261 <romanchik4261@mail.ru>
2021-07-27 19:51:05 +0300

    1 задание

--- script.js
+++ script.js
@@ -1,48 +1,54 @@
 'use strict'
 
 // // 1 задание
+// Первый вариант
+const board = {
+    rowCount: 8,
+    colCount: 8,
+    containerElement: '',
+    cellElement: [],
 
-// const board = {
-//     rowCount: 8,
-//     colCount: 8,
-//     containerElement: '',
-//     cellElement: [],
-
-//     initCells() {
-//         // this.containerElement = document.getElementById('game');
-//         // this.containerElement.innerHTML = '';
-//         this.cellElement = [];
+    initCells() {
+        this.cellElement = [];
     
-//         for (let row = 0; row < this.rowCount; row++) {
-//             const trElem = document.createElement('tr');
-//             this.containerElement.appendChild(trElem);
+        for (let row = 0; row < this.rowCount; row++) {
+            const trElem = document.createElement('tr');
+            this.containerElement.appendChild(trElem);
 
-//             for (let col = 0; col < this.colCount; col++) {
-//                 const td = document.createElement('td');
-//                 trElem.appendChild(td);
+            for (let col = 0; col < this.colCount; col++) {
+                const td = document.createElement('td');
+                trElem.appendChild(td);
 
-//                 this.cellElements.push(td);
-//             }
-//         }
-//     }
-
-// }
-
-// board.initCells()
-
-
-var board = document.createElement('table');
-for (var i = 1; i < 9; i++) {
-    var tr = document.createElement('tr');
-    for (var j = 1; j < 9; j++) {
-        var td = document.createElement('td');
-        if (i%2 == j%2) {
-            td.className = "white";
-        } else {
-            td.className = "black";
+                this.cellElements.push(td);
+            }
         }
-        tr.appendChild(td);
     }
-    board.appendChild(tr);
+
 }
-document.body.appendChild(table);
+
+board.initCells()
+
+// Второй вариант
+
+const desk = {
+    create: document.getElementById('game'),
+
+    createDesk() {
+        for (let i = 1; i < 9; i++) {
+            let tr = document.createElement('tr');
+            for (let j = 1; j < 9; j++) {
+                let td = document.createElement('td');
+                if (i%2 == j%2) {
+                    td.className = "white";
+                } else {
+                    td.className = "black";
+                }
+                tr.appendChild(td);
+            }
+            this.create.appendChild(tr);
+        }
+    }
+}
+ desk.createDesk();
+
+
